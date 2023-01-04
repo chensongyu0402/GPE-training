e13f76c288e2899ff10abfe9828e9912b39be51b
chensungyu <kobe29856907@gmail.com>
2023-01-04 09:06:22 +0800

    OK

---
+++ Zipfs_Law.cpp
@@ -0,0 +1,50 @@
+/*
+    Keyword: None
+    source1(answer): https://github.com/Hikari9/UVa/blob/master/10126%20-%20Zipf's%20Law.cpp
+*/
+#include<stdlib.h>
+#include<map>
+#include<string>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+map<string, int> m;
+map<string, int>:: iterator it;
+string buf, word;
+bool found, f = false;
+
+int main()
+{
+    int n;
+    while(cin >> n) {
+        if(f)printf("\n");
+        f = true;
+        m.clear();
+        while(cin >> buf) {
+            transform(buf.begin(), buf.end(), buf.begin(), ::tolower);
+            if(buf == "endoftext")break;
+            for(int i = 0; i < buf.size(); i++) {
+                if(islower(buf[i])) { // 相當於確認是不是字母
+                    word.clear();
+                    do{
+                        word.push_back(buf[i]);
+                        i++;
+                    }while(i < buf.length() && islower(buf[i]));
+                    m[word]++;
+                }
+            }
+            found = false;
+            for(it = m.begin(); it != m.end(); it ++) {
+                if(it->second == n) {
+                    cout << it->first << endl;
+                    found = true;
+                }
+            }
+
+            if(found == false)
+                cout << "There is no such word." << endl;
+        }
+    }
+    return 0;
+}
